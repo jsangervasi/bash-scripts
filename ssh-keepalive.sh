86a1ab9b9415b330b53e96faf3b194be7a0298a7
jsangervasi <jsangervasi@gmail.com>
2026-02-24 00:24:48 -0800

    Update ssh-keepalive.sh
    moved comments

--- ssh-keepalive.sh
+++ ssh-keepalive.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Author: Joseph M Sangervasi
 # Title: ssh remote session keep-alive
 # Created 02/24/2026
@@ -5,6 +6,5 @@
 # Usage (Linux/Mac): add to .bashrc on remote server to load automatically
 # Function: runs a background process which prints space to remote terminal 
 # window every 5 minutes to keep ssh session alive (EC2, Virtual machines, etc)
-# --- copy lines below --- #
-#!/bin/bash
+# --- copy line below --- #
 while true;do echo " ";sleep 300;done &
